2fa6022ceb6ee3195469c4042944eacadae248d8
Muhammad <ibragim.katalov@mail.ru>
2022-08-26 19:23:36 +0300

    no message

--- Muslim_Everyday_simple/app/src/main/java/com/example/muslim_everyday/recycler_view/SettingsMenuRVAdapter.kt
+++ Muslim_Everyday_simple/app/src/main/java/com/example/muslim_everyday/recycler_view/SettingsMenuRVAdapter.kt
@@ -1,13 +1,8 @@
 package com.example.muslim_everyday.recycler_view
 
-import android.app.NotificationManager
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.example.muslim_everyday.R
-import com.example.muslim_everyday.SettingsMenu
 import com.example.muslim_everyday.data_class.Settings
 import com.example.muslim_everyday.databinding.StringItemBinding
 
@@ -16,14 +11,6 @@ class SettingsMenuRVAdapter(private val settingsList: List<Settings>) : Recycler
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         binding = StringItemBinding.inflate(LayoutInflater.from(parent.context))
-
-        val builder = NotificationCompat.Builder(parent.context)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("Title")
-            .setContentText("Text")
-        val notification = builder.build()
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(1, notification)
 
         return MyViewHolder(binding)
     }
